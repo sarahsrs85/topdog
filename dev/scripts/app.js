83106831c419d27f18210e9f3874d91c7927ed58
Sarah Simpson <sarahsrs85@gmail.com>
2017-11-21 19:17:19 -0500

    adding donuts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Donut from './donut';
+const donuts = [ 'Honey Dip', 'Sourcream Glazed', 'Boston Cream']
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          {donuts.map(donut => {
+            return (
+              <Donut donutName={donut}/>
+            )
+
+          })}
         </div>
       )
     }
 }
-
+//if you use it more than once make it a component 
+// you can have state on other components that app can do it on modules
 ReactDOM.render(<App />, document.getElementById('app'));
